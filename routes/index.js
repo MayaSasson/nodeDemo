69488937a42ff3ddca8538e9538c49b2032ddda2
Maya Sasson <mayokit22@gmail.com>
2021-06-05 04:02:25 -0700

    feat(server): init server

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const nodemailer = require("nodemailer");
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  res.render('index', { title: 'Express' });
+  let transport = nodemailer.createTransport({});
+
+  const message = {
+      from: 'elonmusk@tesla.com',
+      to: 'mayasasson22@gmail.com',
+      subject: 'Design Your Model S | Tesla',
+      html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>',
+  };
+  transport.sendMail(message, (err, info) => {
+      console.log(err, info)
+  });
+  });
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const { Client } = require('pg')
+
+/* GET users listing. */
+router.get('/', async (req, res, next) => {
+  const client = new Client({database: 'postgres', password: 'qwe123', host: 'localhost', port: '5432', user: 'postgres'})
+  await client.connect()
+  const response = await client.query('SELECT * from try')
+  console.log(response.rows[0].id) // Hello world!
+  await client.end()
+  res.send('respond with a resource');
+});
+
+module.exports = router;
