8246dd7dcc01315b2f96da869b6a525ce5862a13
MayaSasson <62448339+MayaSasson@users.noreply.github.com>
2021-06-05 07:54:39 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,9 @@ const { Client } = require('pg')
 router.get('/', async (req, res, next) => {
   const client = new Client({database: 'postgres', password: 'qwe123', host: 'localhost', port: '5432', user: 'postgres'})
   await client.connect()
-  const response = await client.query('SELECT * from try')
-  console.log(response.rows[0].id) // Hello world!
-  await client.end()
+//   const response = await client.query('SELECT * from try')
+//   console.log(response.rows[0].id) // Hello world!
+//   await client.end()
   res.send('respond with a resource');
 });
 
